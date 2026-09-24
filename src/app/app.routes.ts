2493ac44c9fac86949339c3457bb56553fa6c33b
Ritorick0925 <ryuuto26r@gmail.com>
2026-09-24 23:51:02 +0900

    Revert "homeを削除"
    This reverts commit 6347a1886372651c233a26674f045bfc8dd362db.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { StudyBMI } from './study-bmi/study-bmi';
 
 export const routes: Routes = [
-    { path: '' , component: StudyBMI}
+    { path: '' , component: Home},
+    { path: 'study-bmi', component: StudyBMI }
 ];
