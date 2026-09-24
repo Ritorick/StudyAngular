4e52879b86bf5ee999d7d6284db4b9ffc1786f32
Ritorick0925 <ryuuto26r@gmail.com>
2026-09-24 23:16:02 +0900

    BMI測定ページの装飾と、exe(portable)化の設定

--- src/app/study-bmi/study-bmi.ts
+++ src/app/study-bmi/study-bmi.ts
@@ -13,6 +13,10 @@ export class StudyBMI {
   protected readonly title = signal('hello-world-app');
 
   bmi: number = 0;
+  height: number = 0;
+  weight: number = 0;
+  assessment: String = "";
+  color = 'white';
 
   BMIDataInputForm = new FormGroup({
     weight: new FormControl('', [
@@ -28,5 +32,25 @@ export class StudyBMI {
     const height = Number(this.BMIDataInputForm.value.height);
 
     this.bmi = Number((weight / ((height / 100) ** 2)).toFixed(1));
+    this.height = height;
+    if (this.bmi >= 40) {
+      this.assessment = "肥満(4度)";
+      this.color = 'darkred'
+    } else if (this.bmi >= 35) {
+      this.assessment = "肥満(3度)";
+      this.color = 'red'
+    } else if (this.bmi >= 30) {
+      this.assessment = "肥満(2度)";
+      this.color = 'orange'
+    } else if (this.bmi >= 25) {
+      this.assessment = "肥満(1度)";
+      this.color = 'gold'
+    } else if (this.bmi >= 18.5) {
+      this.assessment = "普通体重";
+      this.color = 'yellowgreen'
+    } else {
+      this.assessment = "低体重";
+      this.color = 'blue'
+    }
   }
 }
