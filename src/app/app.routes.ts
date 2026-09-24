6347a1886372651c233a26674f045bfc8dd362db
Ritorick0925 <ryuuto26r@gmail.com>
2026-09-24 23:50:13 +0900

    homeを削除

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
 import { StudyBMI } from './study-bmi/study-bmi';
 
 export const routes: Routes = [
-    { path: '' , component: Home},
-    { path: 'study-bmi', component: StudyBMI }
+    { path: '' , component: StudyBMI}
 ];
