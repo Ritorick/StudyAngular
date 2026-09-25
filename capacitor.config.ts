9e1662d639ef5a69e80db6bf69faa3d43d3385b7
Ritorick0925 <ryuuto26r@gmail.com>
2026-09-25 18:29:50 +0900

    android向けapkをbuild時にexeと同時に生成するようにした。

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ryuut.helloworld',
+  appName: 'Hello World App',
+  webDir: 'dist/hello-world-app/browser'
+};
+
+export default config;
